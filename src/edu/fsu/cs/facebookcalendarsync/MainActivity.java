c7bd4920de9ac665c93c276bc72e6c247a652b1e
James Kobayashi <kobayashijames@gmail.com>
2013-07-10 19:42:21 -0400

    activity_main.xml: added listview MainActivity: added ListView Adapter and checkbox support
    James Kobayashi <kobayashijames@gmail.com>

--- src/edu/fsu/cs/facebookcalendarsync/MainActivity.java
+++ src/edu/fsu/cs/facebookcalendarsync/MainActivity.java
@@ -12,14 +12,22 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
  
 public class MainActivity extends Activity {
 
+	ListView listView1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		/* To create an event on the user's calendar(s), call the following:
 		 *  
 		 * createEvent("James Kobayashi", "2013-07-20");
@@ -33,6 +41,27 @@ public class MainActivity extends Activity {
 		 * 2) Using a calendar widget that syncs multiple calendars will display multiple birthdays
 		 * 3) Currently, no duplicates are being sighted fortunately for a single calendar
 		 */
+		
+		
+		// **** Populate this string array with all names! ****
+		// note: a method to loop and add dynamically will be needed
+		String[] listItems = { "I'm on", "the list!" };
+
+		// Initialize ListView
+		listView1 = (ListView)findViewById(R.id.listView);
+		listView1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, listItems));
+		listView1.setItemsCanFocus(false);
+		listView1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		
+		// Check or Uncheck the box on click
+		listView1.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+			CheckedTextView ctv = (CheckedTextView)arg1;
+			
+			// Any additional stuff to do onClick goes here
+			}
+		});
 	}
 	
 	@Override
