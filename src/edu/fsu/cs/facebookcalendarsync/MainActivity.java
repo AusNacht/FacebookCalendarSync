9ac9406ec38795d87dee7157d6daeca3a24054e1
Aaron Lathrop <ausnacht@gmail.com>
2013-07-14 18:49:48 -0400

    Fixed the duplicate calendar entry issue.
    Signed-off-by: Aaron Lathrop <ausnacht@gmail.com>

--- src/edu/fsu/cs/facebookcalendarsync/MainActivity.java
+++ src/edu/fsu/cs/facebookcalendarsync/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.fsu.cs.facebookcalendarsync;
 
+import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -14,6 +15,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Reminders;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
@@ -24,7 +26,10 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import com.facebook.FacebookRequestError;
 import com.facebook.HttpMethod;
@@ -35,7 +40,6 @@ import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
-
 public class MainActivity extends Activity {
 
 	ListView listView1;
@@ -44,26 +48,40 @@ public class MainActivity extends Activity {
 	private Button submitbutton;
 	private UiLifecycleHelper uiHelper;
 	private Session session;
-	
+
+	/////////////////////////////
+	//Search Constant
+	/////////////////////////////
+	private static final String[] COLS = new String[] {
+			CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART,
+			CalendarContract.Events.DESCRIPTION,
+			CalendarContract.Events.CALENDAR_ID, CalendarContract.Events._ID };
+	/////////////////////////////
+	//end Search Constant
+	/////////////////////////////
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		loginButton = (LoginButton) findViewById(R.id.login);
 		loginButton.setReadPermissions(Arrays.asList("friends_birthday"));
-		loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
-			@Override
-			public void onUserInfoFetched(GraphUser user) {
-				MainActivity.this.user = user;
-				
-				// It's possible that we were waiting for this.user to be populated in order to post a
-				// status update.
-	          	}
-	        });
+		loginButton
+				.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+					@Override
+					public void onUserInfoFetched(GraphUser user) {
+						MainActivity.this.user = user;
+
+						// It's possible that we were waiting for this.user to
+						// be populated in order to post a
+						// status update.
+					}
+				});
 
 		// **** Populate this string array with all names! ****
-		// note: a method to loop and add "name, date" dynamically will be needed
+		// note: a method to loop and add "name, date" dynamically will be
+		// needed
 		String[] listItems = { "James Kobayashi, 07-19", "Guanyu Tian, 07-20" };
 
 		// Initialize ListView
@@ -81,35 +99,35 @@ public class MainActivity extends Activity {
 				CheckedTextView ctv = (CheckedTextView) arg1;
 			}
 		});
-		
+
 		// *** Submit Button ***
-	    submitbutton = (Button) findViewById(R.id.button2);
-	    submitbutton.setOnClickListener(new OnClickListener() {
-	    	public void onClick(View v) {
-	    		
-	    		// Array of checked items and their positions taken
-	    		SparseBooleanArray checkedItems = listView1.getCheckedItemPositions();
-	    	  
-	    		if (checkedItems != null) {
-	    			for (int i = 0; i < checkedItems.size(); i++) {
-	    				if (checkedItems.valueAt(i)) {
-	    					
-	    					// If the item is checked, grab the string
-	    					String friendInfo = listView1.getAdapter()
-	    							.getItem(checkedItems.keyAt(i)).toString();
-	    					
-	    					// Parse the string for name and date
-	    					String[] info = friendInfo.split(",");
-	    				  
-	    					// Enter into calendar (Default 10 minutes)
-	    					createEvent(info[0].trim( ), info[1].trim( ), 10);
-	    				}
-	    			}
-	    		}
-	    	}
-	    });
+		submitbutton = (Button) findViewById(R.id.button2);
+		submitbutton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Array of checked items and their positions taken
+				SparseBooleanArray checkedItems = listView1
+						.getCheckedItemPositions();
+
+				if (checkedItems != null) {
+					for (int i = 0; i < checkedItems.size(); i++) {
+						if (checkedItems.valueAt(i)) {
+
+							// If the item is checked, grab the string
+							String friendInfo = listView1.getAdapter()
+									.getItem(checkedItems.keyAt(i)).toString();
+
+							// Parse the string for name and date
+							String[] info = friendInfo.split(",");
+
+							// Enter into calendar (Default 10 minutes)
+							createEvent(info[0].trim(), info[1].trim(), 10);
+						}
+					}
+				}
+			}
+		});
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -139,19 +157,45 @@ public class MainActivity extends Activity {
 			} catch (Exception e) {
 			}
 
-			/*
-			//search to see if event exists
-			String[] proj = new String[] { Instances._ID, Instances.BEGIN,
-					Instances.END, Instances.EVENT_ID };
-			Cursor cursor = Instances.query(getContentResolver(), proj,
-					theDate -(1000*60*60*24), theDate+(1000*60*60*24), null);
-			//boolean found;
-			//cursor.moveToFirst();
-			while(cursor.moveToNext()){ Log.i("cursor", cursor.getString(1));}//, name + "'s Birthday"
-			if (cursor.getCount() > 0) {//already exists
-			} else {
-			*/
+			// //////////////////////////////////////
+			// search for event
+			// //////////////////////////////////////
+			String mSelectionClause = CalendarContract.Events.TITLE
+					+ " = ? AND " + CalendarContract.Events.DESCRIPTION
+					+ " = ?";// AND " + CalendarContract.Events.DTSTART + " = ?
+								// AND " + CalendarContract.Events.CALENDAR_ID;
+			String[] mSelectionArgs = new String[] { name + "'s Birthday",
+					"Facebook Birthday", };// birthday, calId};
+			String mOrderBy;
+			int[] mListItems;
 
+			mCursor = getContentResolver().query(
+					CalendarContract.Events.CONTENT_URI, COLS,
+					mSelectionClause, mSelectionArgs, null);
+			mCursor.moveToFirst();
+			String title = "N/A";
+			Long start = 0L;
+			Format df = DateFormat.getDateFormat(this);
+			Format tf = DateFormat.getTimeFormat(this);
+			try {
+				title = mCursor.getString(0);
+				start = mCursor.getLong(1);
+			} catch (Exception e) {
+				// ignore
+			}
+			Log.i("find event",
+					title + " on " + df.format(start) + " at "
+							+ tf.format(start));
+			// //////////////////////////////////////
+			// end search for event
+			// //////////////////////////////////////
+
+			////////////////////////////////////////
+			// add event
+			///////////////////////////////////////
+			if (title.equals("N/A")) {
+				// new birthday
+			
 				ContentResolver cr = this.getContentResolver();
 				ContentValues event = new ContentValues();
 
@@ -167,14 +211,6 @@ public class MainActivity extends Activity {
 				event.put(CalendarContract.Events.RRULE, "FREQ=YEARLY");
 				event.put(CalendarContract.Events.HAS_ALARM, 1);
 
-				/*
-				 * Uri eventuri; if (Integer.parseInt(Build.VERSION.SDK) >= 8)
-				 * eventuri =
-				 * cr.insert(Uri.parse("content://com.android.calendar/events"),
-				 * event); else eventuri =
-				 * cr.insert(Uri.parse("content://calendar/events"), event);
-				 */
-
 				// set a 10 minute reminder
 				ContentValues reminders = new ContentValues();
 				Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, event);
@@ -183,41 +219,46 @@ public class MainActivity extends Activity {
 				reminders.put(Reminders.METHOD, Reminders.METHOD_ALERT);
 				reminders.put(Reminders.MINUTES, reminder_minutes);
 				Uri uri2 = cr.insert(Reminders.CONTENT_URI, reminders);
-				// } while (mCursor.moveToNext());
-			//}//end else for query check
+			} else {
+				//birthday already exists
+			}
+			////////////////////////////////////////
+			// end add event
+			///////////////////////////////////////
 		}
 		mCursor.close();
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
-		String fqlQuery = "SELECT uid, name, birthday_date FROM user WHERE uid IN" + "(SELECT uid2 FROM friends WHERE uid1 = me()";
-		
+
+		Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show();
+
+		String fqlQuery = "SELECT uid, name, birthday_date FROM user WHERE uid IN"
+				+ "(SELECT uid2 FROM friends WHERE uid1 = me()";
+
 		Bundle params = new Bundle();
 		params.putString("q", fqlQuery);
-		
+
 		session = Session.getActiveSession();
-		Request request = new Request(session, "/fql", params, HttpMethod.GET, new Request.Callback() {
-			@Override
-			public void onCompleted(Response response) {
-				Log.d("Facebook", "here 1");
-				// TODO Auto-generated method stub
-				FacebookRequestError error = response.getError();
-				Log.d("Facebook", "Result: " + response.toString());
-			}
-		});
-		Request.executeBatchAsync(request); 
+		Request request = new Request(session, "/fql", params, HttpMethod.GET,
+				new Request.Callback() {
+					@Override
+					public void onCompleted(Response response) {
+						Log.d("Facebook", "here 1");
+						// TODO Auto-generated method stub
+						FacebookRequestError error = response.getError();
+						Log.d("Facebook", "Result: " + response.toString());
+					}
+				});
+		Request.executeBatchAsync(request);
 
 	}
-	
+
 	/*
-	 * Might be used later.
-	 * private void updateUI() {
-	 * Session session = Session.getActiveSession();
-	 * boolean enableButtons = (session != null && session.isOpened());
-	 * //submitbutton.setEnabled(enableButtons);	        
-	 * }
+	 * Might be used later. private void updateUI() { Session session =
+	 * Session.getActiveSession(); boolean enableButtons = (session != null &&
+	 * session.isOpened()); //submitbutton.setEnabled(enableButtons); }
 	 */
 }
